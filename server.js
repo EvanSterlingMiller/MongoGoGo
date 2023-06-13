a772f0c2058269b7e5be0f75197ec38135aa784a
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-06-13 18:35:24 -0400

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.use(require('./routes'))
+
+app.listen(PORT, ()=> console.log(`Connected on localhost:${PORT}`)
