3a2767a0c91b3e7d8a4cc1ca95c73f8c868c1d36
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-06-17 19:06:27 -0400

    get all thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,2 +1,18 @@
 const {Thought, User} = require('..models')
 
+const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .populate({
+                path: "reactions",
+                select: "-__v"
+            })
+            .select("-__v")
+            .sort({_id: -1})
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => {
+                console.log(err)
+                res.status(400).json(err)
+            })
+    }
+}
